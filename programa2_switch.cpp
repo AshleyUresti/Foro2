0a749afd0ec1bc79d7da0f0239d23b5a74d78527
URESTI MARTINEZ ASHLEY JANETH <zS25025286@estudiantes.uv.mx>
2025-10-06 09:31:07 -0600

    Se agregó el programa2

--- programa2_switch.cpp
+++ programa2_switch.cpp
@@ -5,5 +5,34 @@
 using namespace std;
 
 int main() {
-    
+    int opcion;
+    do {
+        cout << "===Menú===" << endl;
+        cout << "1) Torta" << endl;
+        cout << "2) Palomitas" << endl;
+        cout << "3) Agua" << endl;
+        cout << "4) Salir" << endl;
+
+        cout << "Selecciona el producto: ";
+        cin >> opcion;
+
+        switch (opcion) {
+            case 1:
+                cout << "Tortas: $45" << endl;
+                break;
+            case 2:
+                cout << "Palomitas: $30" << endl;
+                break;
+            case 3:
+                cout << "Aguas 600ml: $15" << endl;
+                break;
+            case 4:
+                cout << "Saliendo del menú..." << endl;
+                break;
+            default:
+                cout << "Opción inválida. Intenta de nuevo." << endl;
+        }
+    } while (opcion != 4);
+
+    return 0;
 }
