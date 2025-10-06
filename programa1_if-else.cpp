ef84a3d726badfcdb2794278ec8a4550b0f0fa9d
URESTI MARTINEZ ASHLEY JANETH <zS25025286@estudiantes.uv.mx>
2025-10-06 09:22:02 -0600

    archivo caso1

---
+++ programa1_if-else.cpp
@@ -0,0 +1,30 @@
+//Elabore programas que muestre un menú de productos y retorne el precio del producto seleccionado:
+
+//Programa 1: Utilice if-else
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int opcion;
+    do {
+        cout << "====Menú====" << endl;
+        cout << "1) Torta" << endl;
+        cout << "2) Palomitas" << endl;
+        cout << "3) Agua" << endl;
+        cout << "4) Salir" << endl;
+        cin >> opcion;
+
+        if (opcion == 1 ) {
+            cout << "Tortas: $45" << endl;
+        } else if (opcion == 2 ) {
+            cout << "Palomitas: $10" << endl;
+        } else if (opcion == 3) {
+            cout << "Aguas 600ml: $15" << endl;
+        }
+
+    } while (opcion != 4);
+    
+    return 0;
+}
+
---
+++ programa2_switch.cpp
@@ -0,0 +1,9 @@
+//Elabore programas que muestre un menú de productos y retorne el precio del producto seleccionado:
+//Programa 2: Utilice switch
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+}
